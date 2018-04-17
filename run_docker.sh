1fc4029762c810225c7257a93131d959dc4719ec
Alex Golts <alexgbiz@gmail.com>
2018-04-17 11:03:37 +0300

    more secure run script. allow access only to local ip

--- run_docker.sh
+++ run_docker.sh
@@ -2,9 +2,12 @@
 
 ################################################################################
 # This is supposedly insecure... use Google to find a safer solution if you like
-xhost +local:root
+#xhost +local:root
 ################################################################################
+#IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 
+IP=$(hostname -I|cut -f1 -d ' ')
+xhost + $IP
 
 nvidia-docker run \
 	-it \
