6a68c14cb7fc04cb77f426f2e237b231d34406ea
Alex Golts <alex@golts.net>
2019-11-12 00:23:08 -0800

    starting from docker v19.03 gpus are supported natively, no need for nvidia-docker command

---
+++ run_docker_new.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+################################################################################
+# This is supposedly insecure... use Google to find a safer solution if you like
+#xhost +local:root
+################################################################################
+#IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+
+IP=$(hostname -I|cut -f1 -d ' ')
+
+xhost + $IP
+
+git_name=$(git config --global user.name)
+git_email=$(git config --global user.email)
+
+docker run --gpus all \
+	-it \
+	-e DISPLAY=$DISPLAY \
+	-e QT_X11_NO_MITSHM=1 \
+	-e LUID=$(id -u) \
+        -e LGID=$(id -g) \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v /home/$USER:/home/$USER \
+	-v /home/$USER/.Xauthority:/home/dluser/.Xauthority:rw \
+	-v /media:/media \
+	-v /data/$USER:/data/$USER \
+	--net=host \
+	--shm-size=16G \
+	--name ${USER}_$(date +%d_%m_%Y_%H_%M_%S) \
+	--privileged \
+        --rm \
+	"$@" bash -c "git config --global user.name '$git_name' && git config --global user.email '$git_email' && bash"
